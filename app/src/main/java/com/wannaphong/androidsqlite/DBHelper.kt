db5501146ad50269493ed235b66472e35622e48a
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2020-01-17 10:46:46 +0700

    Create DBHelper.kt

---
+++ app/src/main/java/com/wannaphong/androidsqlite/DBHelper.kt
@@ -0,0 +1,21 @@
+package com.wannaphong.androidsqlite
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DBHelper(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
